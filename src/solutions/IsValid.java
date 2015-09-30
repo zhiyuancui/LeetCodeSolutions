22df1d845c0fbc6ccacba3f3d4f8cdf72241003d
Zhiyuan <zhiyuan.richard.cui@gmail.com>
2015-09-30 18:14:22 -0400

    solve the problem

---
+++ src/solutions/IsValid.java
@@ -0,0 +1,49 @@
+package solutions;
+
+import java.util.Stack;
+
+public class IsValid {
+	public boolean isValid(String s) {
+	     
+	     if( s == null || s.length() == 0 )
+	     {
+	         return false;
+	     }  
+	     
+	     Stack<Character> stack = new Stack<Character>();
+	     
+	     for(int i = 0; i < s.length(); i++)
+	     {
+	         if(s.charAt(i) =='('||s.charAt(i) =='{'||s.charAt(i) =='[')
+	         {
+	             stack.push( s.charAt(i) );
+	         }
+	         else
+	         {
+	             if( !stack.isEmpty() )
+	             {
+	                 if( check( stack.peek(), s.charAt(i) ) )
+	                 {
+	                     stack.pop();
+	                 }
+	                 else
+	                 {
+	                     return false;
+	                 }
+	             }
+	             else
+	             {
+	                 return false;
+	             }
+	         }
+	     }
+	     
+	     return stack.isEmpty();
+	     
+	    }
+	    
+	    private boolean check(Character c1, Character c2 )
+	    {
+	        return (c1 == '(' && c2 ==')') || (c1 == '[' && c2 ==']' )|| (c1 == '{' && c2 =='}');
+	    }
+}
--- src/solutions/MyPow.java
+++ src/solutions/MyPow.java
@@ -1,5 +1,36 @@
 package solutions;
 
 public class MyPow {
+public double myPow(double x, int n) {
+        
+        if( n == 0 )
+        {
+            return 1;
+        }
+        
+        if( n == 1 )
+        {
+            return x;
+        }
+        
+        boolean neg = false;
+        if( n < 0 )
+        {
+            neg = true;
+            n = -n;
+        }
+        
+        double t1 = myPow(x, n/2 );
+        double t2 = myPow(x, n- (n/2)*2);
+        
+        return neg ? 1/(t1*t1*t2): t1*t1*t2;
+        
+    }
 
+	public static void main(String[] args)
+	{
+		MyPow m = new MyPow();
+		
+		m.myPow(34.00515, -3);
+	}
 }
