544fc5b027cc838fe8b4616cf14e4e2c99119310
Zhiyuan <zhiyuan.richard.cui@gmail.com>
2016-03-23 17:18:59 -0400

    add new solutions

--- src/solutions/Hindex.java
+++ src/solutions/Hindex.java
@@ -1,5 +1,7 @@
 package solutions;
 
+import java.util.Arrays;
+
 public class Hindex {
 	public int hIndex(int[] citations) {
         if( citations == null || citations.length == 0 ){
@@ -23,5 +25,52 @@ public class Hindex {
         }
         
         return hIndex;
+    }
+	
+	/**
+	 * H-Index not sorted
+	 * @param citations
+	 * @return
+	 */
+	public int hIndex2(int[] citations) {
+        if(citations==null || citations.length==0 )
+        return 0;
+
+    //if array has 1 element greater than 0, return 1    
+    if(citations.length==1 && citations[0]>=1)
+        return 1;
+
+    //sort the array
+    Arrays.sort(citations);
+
+    //variable to keep track of length of array, hIndex
+    int length=citations.length;
+    int hIndex=0;
+
+    //for loop to traverse the array
+    for(int i=length-1;i>=0;i--)
+    {
+        //if value is present in array
+        if(length-i==citations[i])
+        {
+            hIndex=citations[i];
+        }
+        else
+        {
+            //if value is not present in array
+            if(i>0 &&citations[i-1]<=length-i && citations[i]>=length-i)
+                hIndex=length-i;
+        }
+
+
+    }
+
+    //first element is greater than 0, and hash index is 0, return the length of the array as answer
+    if(citations.length>1 && citations[0]>citations.length && hIndex==0)
+        hIndex=citations.length;
+
+
+    //return the answer
+    return hIndex;
     }
 }
---
+++ src/solutions/WordPattern.java
@@ -0,0 +1,37 @@
+package solutions;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class WordPattern {
+	public boolean wordPattern(String pattern, String str) {
+        String[] parts = str.split("\\s+");
+        if( parts.length == 0 ){
+            return false;
+        }
+        
+        if( parts.length != pattern.length() ){
+            return false;
+        }
+        
+        HashMap<Character, String> map = new HashMap<Character,String>();
+        HashSet<String> set = new HashSet<String>();
+        for(int i = 0; i < pattern.length(); i++){
+            Character letter = pattern.charAt(i);
+            if( map.containsKey( letter ) ){
+                if( !parts[ i ].equals( map.get( letter) ) ){
+                    return false;
+                }
+            }else{
+                if( !set.contains( parts[i] ) ){
+                    map.put(letter, parts[i]);
+                    set.add( parts[i] );
+                }else{
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+}
