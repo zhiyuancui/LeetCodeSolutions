258f553ff2ca454e657a651877092834d3e53fff
Zhiyuan <zhiyuan.richard.cui@gmail.com>
2016-04-14 15:54:12 -0400

    add new solutions

--- src/solutions/Anagram.java
+++ src/solutions/Anagram.java
@@ -11,7 +11,7 @@ public class Anagram {
 	 * For example, s = "anagram", t = "nagaram", return true.
 	 * s = "rat", t = "car", return false.
 	 */
-public boolean isAnagram(String s, String t) {
+	public boolean isAnagram(String s, String t) {
         
         if( s == null && t == null )
         {
@@ -37,4 +37,34 @@ public boolean isAnagram(String s, String t) {
         
         return new String( temp );
     }
+    
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new ArrayList<List<String>>();
+        
+        if( strs == null || strs.length == 0 ){
+            return result;
+        }
+        
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        
+        for(String s : strs ){
+            String code = encode(s);
+            if( map.containsKey( code ) ){
+                List<String> temp = map.get( code );
+                temp.add( s );
+            }else{
+                List<String> anagram =  new ArrayList<String>();
+                map.put(code, anagram);
+            }
+        }
+        
+        for(List<String> anagrams : map.values() ){
+            Collections.sort( anagrams );
+            result.add( anagrams );
+        }
+        
+        return result;
+    }
+    
+    
 }
--- src/solutions/ReverseBetween.java
+++ src/solutions/ReverseBetween.java
@@ -5,37 +5,28 @@ import util.ListNode;
 public class ReverseBetween {
 
 	public ListNode reverseBetween(ListNode head, int m, int n) {
-        
-        
-        if( head == null || m >= n )
-        {
+        		
+        if( head == null || head.next == null ){
             return head;
         }
         
         ListNode dummy = new ListNode(0);
-        
         dummy.next = head;
         
-        ListNode begin = dummy;
-        
-        for(int i = 0; i < m - 1; i++ )
-        {
-            if( begin == null )
-            {
+        ListNode prev = dummy;
+        for(int i = 0; i < m - 1; i++ ){
+            if( prev == null ){
                 return null;
             }
-            begin = begin.next;
+            prev = prev.next;
         }
         
-        ListNode prev = begin.next;
-        
-        ListNode cur = prev.next;
-        for(int i = m; i < n; i++)
-        {
-            prev.next = cur.next;
-            cur.next = begin.next;
-            begin.next = cur;
-            cur = prev.next;
+        ListNode tail = prev.next;
+        for(int i = m; i < n; i++){
+            ListNode cur = tail.next;
+            tail.next = cur.next;
+            cur.next = prev.next;
+            prev.next = cur;
         }
         
         return dummy.next;
