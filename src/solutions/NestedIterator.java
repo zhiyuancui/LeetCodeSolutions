f0b84ccec07fcb1e740ce58bf772ac08af006fed
Zhiyuan <Zhiyuan@Lenovo-PC.fios-router.home>
2016-04-17 15:13:47 -0400

    add new solutions

---
+++ src/solutions/NestedIterator.java
@@ -0,0 +1,49 @@
+package solutions;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import util.NestedInteger;
+
+public class NestedIterator implements Iterator<Integer> {
+
+    List<Integer> list;
+    
+    Iterator<Integer> it;
+    
+    public NestedIterator(List<NestedInteger> nestedList) {
+        
+        list = new LinkedList<Integer>();
+        flatten( nestedList );
+        it = list.iterator();
+    }
+
+    private void flatten(List<NestedInteger> nestedList){
+        for(NestedInteger i : nestedList){
+            if( i.isInteger() ){
+                list.add( i.getInteger() );  
+            }else{
+                flatten( i.getList() );
+            }
+        }
+    }
+
+    @Override
+    public Integer next() {
+        if( it != null ){
+            return it.next();
+        }else{
+            return 0;
+        }
+    }
+
+    @Override
+    public boolean hasNext() {
+        if( it != null ){
+            return it.hasNext();
+        }else{
+            return false;
+        }
+    }
+}
---
+++ src/util/NestedInteger.java
@@ -0,0 +1,9 @@
+package util;
+
+import java.util.List;
+
+public interface NestedInteger {
+	public boolean isInteger();
+	public Integer getInteger();
+	public List<NestedInteger> getList();
+}
