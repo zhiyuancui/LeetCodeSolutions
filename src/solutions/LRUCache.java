1128dbac4da436e14ec3002a778efb43c2f5bf2f
Zhiyuan <Zhiyuan@Lenovo-PC.fios-router.home>
2016-04-26 00:24:08 -0400

    add new solutions

---
+++ src/solutions/LRUCache.java
@@ -0,0 +1,41 @@
+package solutions;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LRUCache {
+    LinkedHashMap<Integer, Integer> cache;
+    public LRUCache(int capacity) {
+      cache = new LinkedHashMap<Integer, Integer>(){
+
+      @Override
+      protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+        if(cache.size() > capacity) {
+          return true;
+        }
+        return false;
+      }
+    };
+   }
+
+    public int get(int key) {
+      if(cache.containsKey(key)) {
+        int value = cache.get(key);
+        moveToFront(key);
+        return value;
+      }
+      return -1;
+    }
+
+    void moveToFront(int key) {
+
+      int val = cache.get(key);
+      cache.remove(key);
+      cache.put(key,val);
+    }
+
+    public void set(int key, int value) { 
+      cache.put(key, value);
+      moveToFront(key);
+    }
+}
