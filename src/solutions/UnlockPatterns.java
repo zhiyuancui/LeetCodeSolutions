e7f3c52d3ae1a2d07542efa0714a51a1d652108b
Zhiyuan <Zhiyuan@Lenovo-PC.fios-router.home>
2016-05-23 14:57:01 -0400

    add new solution

---
+++ src/solutions/UnlockPatterns.java
@@ -0,0 +1,58 @@
+package solutions;
+
+/**
+ * Reference to : https://leetcode.com/discuss/104311/java-easy-understand-dfs-solution
+ * 
+ * @author Zhiyuan
+ *
+ */
+public class UnlockPatterns {
+	
+	private int count = 0;
+	public int numberOfPatterns(int m, int n) {
+        boolean[][] keyboard = new boolean[3][3];
+        
+        for(int i = 0; i < 3; i++){
+        	for(int j = 0; j < 3; j++){
+        		keyboard[i][j] = false;
+        	}
+        }
+        
+        for(int p = m; p <= n; p++){
+        	for(int i = 0; i < 3; i++){
+        		for(int j = 0; j < 3; j++){
+        			keyboard[i][j] = true;
+        			dfs(keyboard,p-1,i,j);
+        			keyboard[i][j] = false;
+        		}
+        	}
+        }
+        
+        return count;
+    }
+	
+	private void dfs(boolean[][] keyboard, int len, int x, int y ){
+		if( len == 0 ){
+			count++;
+			return;
+		}
+		
+		for(int i = 0; i < 3; i++){
+			for(int j = 0; j <3; j++){
+				if( keyboard[i][j] ||
+						( x == i && Math.abs(y-j) > 1 && !keyboard[x][1]) || 
+						( y == j && Math.abs(x-i) > 1 && !keyboard[1][y]) ||
+						( (x + y == i + j) && Math.abs(x-i) > 1 && !keyboard[1][1]) ||
+						( (x - y == i - j) && Math.abs(x-i) > 1 && !keyboard[1][1]) ||
+						(( x ==i && y == j))
+						){
+					continue;
+				}else{
+					keyboard[i][j] = true;
+					dfs(keyboard,len-1,i,j);
+					keyboard[i][j] = false;
+				}
+			}
+		}
+	}
+}
