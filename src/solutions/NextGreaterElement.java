a3769a6f601bea4270f9b971b55ca6c063b2723a
zhiyuan.cui <zhiyuan.richard.cui@gmail.com>
2017-06-14 00:33:53 -0400

    add new solution

--- src/solutions/NextGreaterElement.java
+++ src/solutions/NextGreaterElement.java
@@ -1,5 +1,37 @@
 package solutions;
 
+import java.util.Stack;
+
 public class NextGreaterElement {
+
+	/**
+	 * Next Greater Element II
+	 * @param nums
+	 * @return
+	 */
+	public int[] nextGreaterElements(int[] nums) {
+        
+        int n = nums.length;
+        int[] result = new int[n];
+        
+        Stack<Integer> stack = new Stack<>();
+        for(int i = n-1; i >= 0; i--){
+            stack.push(i);
+        }
+        
+        for(int i = n - 1; i >= 0; i--){
+            result[i] = -1;
+            while( !stack.isEmpty() && nums[ stack.peek()] <= nums[i] ){
+                stack.pop();
+            }
+            
+            if( !stack.isEmpty() ){
+                result[i] = nums[ stack.peek() ];
+            }
+            stack.add(i);
+        }
+        
+        return result;
+    }
 
 }
