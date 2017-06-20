b2630732d2f15c941a1435af6fc29277ce647740
zhiyuan.cui <zhiyuan.richard.cui@gmail.com>
2017-06-20 13:41:03 -0400

    add new solutions

--- src/solutions/ValidPerfectSquare.java
+++ src/solutions/ValidPerfectSquare.java
@@ -3,7 +3,7 @@ package solutions;
 public class ValidPerfectSquare {
 
 	/**
-	 * Reference to https://discuss.leetcode.com/topic/49325/a-square-number-is-1-3-5-7-java-code/2
+	 * Reference to https://discuss.leetcode.com/topic/49325/a-square-number-is-1-3-5-7-java-code
 	 * @param num
 	 * @return
 	 */
@@ -16,5 +16,20 @@ public class ValidPerfectSquare {
         }
         return num == 0;
         
+    }
+	
+	public boolean isPerfectSquare2(int num) {
+        int low = 1, high = num;
+        while (low <= high) {
+            long mid = (low + high) >>> 1;
+            if (mid * mid == num) {
+                return true;
+            } else if (mid * mid < num) {
+                low = (int) mid + 1;
+            } else {
+                high = (int) mid - 1;
+            }
+        }
+        return false;
     }
 }
