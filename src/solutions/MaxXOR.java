5e6ad44c43a7a461b4d90fc113f007e6e3c3a54b
zhiyuan.cui <zhiyuan.richard.cui@gmail.com>
2017-07-31 18:31:01 -0400

    update solution

--- src/solutions/MaxXOR.java
+++ src/solutions/MaxXOR.java
@@ -4,6 +4,11 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class MaxXOR {
+	/**
+	 * Reference to :https://discuss.leetcode.com/topic/63213/java-o-n-solution-using-bit-manipulation-and-hashmap/4
+	 * @param nums
+	 * @return
+	 */
 	public int findMaximumXOR(int[] nums) {
         int max = 0, mask = 0;
         for(int i = 31; i >= 0; i--){
@@ -19,6 +24,54 @@ public class MaxXOR {
                     break;
                 }
             }
+        }
+        return max;
+    }
+	
+	
+	class Trie {
+        Trie[] children;
+        public Trie() {
+            children = new Trie[2];
+        }
+    }
+    
+	/**
+	 * Trie Tree Solution
+	 * Still using a^b = c, a^c =b
+	 * @param nums
+	 * @return
+	 */
+    public int findMaximumXOR2(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return 0;
+        }
+        // Init Trie.
+        Trie root = new Trie();
+        for(int num: nums) {
+            Trie curNode = root;
+            for(int i = 31; i >= 0; i --) {
+                int curBit = (num >>> i) & 1;
+                if(curNode.children[curBit] == null) {
+                    curNode.children[curBit] = new Trie();
+                }
+                curNode = curNode.children[curBit];
+            }
+        }
+        int max = Integer.MIN_VALUE;
+        for(int num: nums) {
+            Trie curNode = root;
+            int curSum = 0;
+            for(int i = 31; i >= 0; i--) {
+                int curBit = (num >>> i) & 1;
+                if(curNode.children[curBit ^ 1] != null) {
+                    curSum += (1 << i);
+                    curNode = curNode.children[curBit ^ 1];
+                }else {
+                    curNode = curNode.children[curBit];
+                }
+            }
+            max = Math.max(curSum, max);
         }
         return max;
     }
