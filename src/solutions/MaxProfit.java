13e479cb9c57a314d08f6add7bccddc45434b6ed
Zhiyuan <Zhiyuan@Lenovo-PC>
2017-08-02 00:06:15 -0400

    add and update solution

--- src/solutions/MaxProfit.java
+++ src/solutions/MaxProfit.java
@@ -147,4 +147,36 @@ public class MaxProfit {
 	    }
 	    return sell;
     }
+	
+	/**
+	 * Best Time to Buy and Sell Stock with Cool down 
+	 * another solution
+	 * @param prices
+	 * @return
+	 */
+	public int maxProfit6(int[] prices) {
+        if( prices == null || prices.length <= 1 ) {
+            return 0;
+        }
+        
+        int[] buy = new int[prices.length];
+        int[] sell = new int[prices.length];
+        int[] cool = new int[prices.length];
+        
+        buy[0]= -prices[0];
+        sell[0] = 0;
+        cool[0] = 0;
+        
+        for(int i = 1; i < prices.length; i++) {
+            buy[i] = Math.max(cool[i-1]-prices[i], buy[i-1]);
+            sell[i] = Math.max(buy[i-1]+prices[i], sell[i-1]);
+            cool[i] = Math.max(sell[i-1], Math.max(buy[i-1], cool[i-1]));
+        }
+        
+        System.out.println( sell[prices.length - 1 ] );
+        System.out.println( buy[prices.length - 1 ] );
+        System.out.println( cool[prices.length - 1 ] );
+        
+        return Math.max( sell[prices.length - 1 ], Math.max(buy[prices.length - 1 ],cool[prices.length - 1 ]) );
+    }
 }
