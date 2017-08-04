8fa73bbc3c2eb786bdf61096b464b81075913813
Zhiyuan <Zhiyuan@Lenovo-PC>
2017-08-04 15:31:25 -0400

    add and update solution

--- src/solutions/IntegerReplacement.java
+++ src/solutions/IntegerReplacement.java
@@ -1,18 +1,27 @@
 package solutions;
 
 public class IntegerReplacement {
+	/**
+	 * Reference to :http://blog.csdn.net/tingting256/article/details/52558884
+	 * @param n
+	 * @return
+	 */
 	public int integerReplacement(int n) {
+        long N = n;
         int step = 0;
-        while (n != 1) {
-            if ((n & 1) == 0) {
-                n >>>= 1;
-            } else if (n == 3 || ((n >>> 1) & 1) == 0) {
-                --n;
-            } else {
-                ++n;
-            }
-            step++;
+        while( N != 1 ) {
+        	if( N % 2 == 0 ) {
+        		N >>=1;
+        	} else {
+        		if( N == 3 ) {
+        			step += 2;
+        			break;
+        		}
+        		N = (N&2) == 2 ? N + 1 : N - 1;
+        	}
+        	step++;
         }
+        
         return step;
     }
 }
