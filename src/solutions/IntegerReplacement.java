697e3c2e9b9b9b4e5b8eff712de50390dfa5081c
zhiyuan.cui <zhiyuan.richard.cui@gmail.com>
2017-08-16 17:58:38 -0400

    add and update solution

--- src/solutions/IntegerReplacement.java
+++ src/solutions/IntegerReplacement.java
@@ -10,16 +10,16 @@ public class IntegerReplacement {
         long N = n;
         int step = 0;
         while( N != 1 ) {
-        	if( N % 2 == 0 ) {
-        		N >>=1;
-        	} else {
-        		if( N == 3 ) {
-        			step += 2;
-        			break;
-        		}
-        		N = (N&2) == 2 ? N + 1 : N - 1;
-        	}
-        	step++;
+	        	if( N % 2 == 0 ) {
+	        		N >>=1;
+	        	} else {
+	        		if( N == 3 ) {
+	        			step += 2;
+	        			break;
+	        		}
+	        		N = (N&2) == 2 ? N + 1 : N - 1;
+	        	}
+	        	step++;
         }
         
         return step;
