b094ecda03616d49edb3d5ec53df2316eb3de5f7
Zhiyuan <Zhiyuan@Lenovo-PC>
2017-08-18 00:03:53 -0400

    add and update solution

--- src/solutions/EditDistance.java
+++ src/solutions/EditDistance.java
@@ -27,27 +27,5 @@ public class EditDistance {
     }
 	
 	
-	/**
-	 * One Edit Distance
-	 * 
-	 * Given two strings S and T, determine if they are both one edit distance apart.
-	 * 
-	 * Reference to :https://leetcode.com/discuss/71071/my-clear-java-solution-with-explanation
-	 * 
-	 */
-	public boolean isOneEditDistance(String s, String t) {
-        for(int i = 0; i < Math.min(s.length(), t.length()); i++){
-        	if( s.charAt(i) != t.charAt(i) ){
-        		if( s.length() == t.length() ){
-        			return s.substring(i+1).equals(t.substring(i+1));
-        		}else if( s.length() < t.length() ){
-        			return s.substring(i).equals(t.substring(i+1));
-        		}else{
-        			return t.substring(i).equals(s.substring(i+1));
-        		}
-        	}
-        }
-        
-        return Math.abs(s.length() - t.length() ) == 1;
-    }
+	
 }
---
+++ src/solutions/FindDerangement.java
@@ -0,0 +1,11 @@
+package solutions;
+
+public class FindDerangement {
+	private static final int M = 1000000007;
+    public int findDerangement(int n) {
+        long ans = 1;
+        for (int i = 1; i <= n; i++) 
+            ans = (i * ans % M + (i % 2 == 0 ? 1 : -1)) % M;
+        return (int) ans;
+    }
+}
