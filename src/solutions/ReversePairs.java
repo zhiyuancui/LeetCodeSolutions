437e7643e40fc1fe0bd6250e98411792a80b3ed3
zhiyuan.cui <zhiyuan.richard.cui@gmail.com>
2017-08-21 17:00:34 -0400

    add and update solution

--- src/solutions/ReversePairs.java
+++ src/solutions/ReversePairs.java
@@ -9,18 +9,18 @@ public class ReversePairs {
 	 * @param nums
 	 * @return
 	 */
-	 public int reversePairs(int[] nums) {
+	public int reversePairs(int[] nums) {
 	        return mergeSort(nums, 0, nums.length-1);
 	    }
-	    private int mergeSort(int[] nums, int s, int e){
-	        if(s>=e) return 0; 
-	        int mid = s + (e-s)/2; 
-	        int cnt = mergeSort(nums, s, mid) + mergeSort(nums, mid+1, e); 
-	        for(int i = s, j = mid+1; i<=mid; i++){
-	            while(j<=e && nums[i]/2.0 > nums[j]) j++; 
-	            cnt += j-(mid+1); 
-	        }
-	        Arrays.sort(nums, s, e+1); 
-	        return cnt; 
-	    }
+    private int mergeSort(int[] nums, int s, int e){
+        if(s>=e) return 0; 
+        int mid = s + (e-s)/2; 
+        int cnt = mergeSort(nums, s, mid) + mergeSort(nums, mid+1, e); 
+        for(int i = s, j = mid+1; i<=mid; i++){
+            while(j<=e && nums[i]/2.0 > nums[j]) j++; 
+            cnt += j-(mid+1); 
+        }
+        Arrays.sort(nums, s, e+1); 
+        return cnt; 
+    }
 }
