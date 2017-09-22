b2a22b326e2c79f5a433af3f3c95c980e39d3f59
Zhiyuan <Zhiyuan@Lenovo-PC>
2017-09-22 00:03:16 -0400

    add and update solution

--- src/solutions/CountAndSay.java
+++ src/solutions/CountAndSay.java
@@ -4,48 +4,38 @@ public class CountAndSay {
 
 public String countAndSay(int n) {
         
-        if( n < 1 )
-        {
-            return "";
-        }
-        
-        int times = 1;
-        
-        String result = "1";
-        while( times < n )
-        {
-            String old = result;
-            int count = 0;
-            char token = old.charAt(0);
-            String next = "";
-            for(int i = 0; i < old.length(); i++)
-            {
-                if( i == 0 )
-                {
-                    token = old.charAt(0);
-                    count++;
-                    continue;
-                }
-                
-                if( token == old.charAt(i) )
-                {
-                    count++;
-                }
-                else
-                {
-                    next += Integer.toString(count) + Character.toString( token );
-                    count=1;
-                    token = old.charAt(i);
-                }
-            }
-            
-            next += Integer.toString(count) + Character.toString( token );
-            
-            result = next;
-            times++;
-        }
-        
-        return result;
+	if( n < 1 ) {
+        return "";
     }
+    
+
+    
+    int time = 1;
+    String result = "1";
+    
+    while( time < n ) {
+        StringBuilder next = new StringBuilder();
+        int count = 0;
+        char token = 'a';
+        for(int i = 0; i < result.length(); i++ ) {
+            
+            if( token != result.charAt(i) ) {
+                if( count != 0 ){
+                    next.append(count).append(token);
+                }
+                count = 1;
+                token = result.charAt(i);
+            } else {
+                count++;
+            }
+        }
+        
+        next.append(count).append(token);
+        result = next.toString();
+        time++;
+    }
+    
+    return result;
+}
 
 }
