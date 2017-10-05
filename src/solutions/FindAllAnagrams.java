7fc4eef7ae56dc4540b51523728d18e9429b4c48
zhiyuan.cui <zhiyuan.richard.cui@gmail.com>
2017-10-05 18:20:07 -0400

    add and update solution

--- src/solutions/FindAllAnagrams.java
+++ src/solutions/FindAllAnagrams.java
@@ -7,35 +7,42 @@ public class FindAllAnagrams {
 
 	/**
 	 * Reference to: https://discuss.leetcode.com/topic/64434/shortest-concise-java-o-n-sliding-window-solution
+	 * Time Complexity : O(N)
+	 * Space Complexity : O(256)
 	 * @param s
 	 * @param p
 	 * @return
 	 */
 	public List<Integer> findAnagrams(String s, String p) {
-		List<Integer> list = new ArrayList<>();
-        if (s == null || s.length() == 0 || p == null || p.length() == 0) {
-            return list;
+		if( s == null || p == null || s.length() == 0 || p.length() == 0 ) {
+            return new ArrayList<>();
         }
-        int[] hash = new int[256]; 
-        
-        for (char c : p.toCharArray()) {
+        int[] hash = new int[256];
+        for( char c : p.toCharArray() ) {
             hash[c]++;
         }
         
+        List<Integer> list = new ArrayList<>();
         int left = 0, right = 0, count = p.length();
-        while (right < s.length()) {
-        
-            if (hash[s.charAt(right++)]-- >= 1) {
+        while( right < s.length() ) {
+            if( hash[s.charAt(right)] >= 1 ) {
                 count--;
             }
-            if (count == 0) {
+            hash[ s.charAt(right) ]--;
+            right++;
+            
+            if( count == 0 ) {
                 list.add(left);
             }
-            
-            if (right - left == p.length() && hash[s.charAt(left++)]++ >= 0) {
-                count++;
+            if( right - left == p.length() ){ 
+                if( hash[ s.charAt(left) ] >= 0 ) {
+                    count++;
+                }
+                hash[ s.charAt(left) ]++;
+                left++;
             }
         }
+        
         return list;
     }
 	
