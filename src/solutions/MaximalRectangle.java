dca4508ec3ab18597cb20f3e01f3797b323133ec
Zhiyuan <Zhiyuan@Lenovo-PC>
2017-11-10 17:23:42 -0500

    add and update solution

--- src/solutions/MaximalRectangle.java
+++ src/solutions/MaximalRectangle.java
@@ -4,45 +4,52 @@ import java.util.Stack;
 
 public class MaximalRectangle {
 	public int maximalRectangle(char[][] matrix) {
-	    if(matrix == null || matrix.length == 0 || matrix[0].length == 0) return 0;
-	    
-	    int[] height = new int[matrix[0].length];
-	    for(int i = 0; i < matrix[0].length; i ++){
-	        if(matrix[0][i] == '1') height[i] = 1;
-	    }
-	    int result = largestInLine(height);
-	    for(int i = 1; i < matrix.length; i ++){
-	        resetHeight(matrix, height, i);
-	        result = Math.max(result, largestInLine(height));
-	    }
-	    
-	    return result;
-	}
-
-	private void resetHeight(char[][] matrix, int[] height, int idx){
-	    for(int i = 0; i < matrix[0].length; i ++){
-	        if(matrix[idx][i] == '1') height[i] += 1;
-	        else height[i] = 0;
-	    }
-	}    
-
-	public int largestInLine(int[] height) {
-	    if(height == null || height.length == 0) return 0;
-	    int len = height.length;
-	    Stack<Integer> s = new Stack<Integer>();
-	    int maxArea = 0;
-	    for(int i = 0; i <= len; i++){
-	        int h = (i == len ? 0 : height[i]);
-	        if(s.isEmpty() || h >= height[s.peek()]){
-	            s.push(i);
-	        }else{
-	            int tp = s.pop();
-	            int he = height[tp];
-                int w = s.isEmpty() ? i : i - 1 - s.peek();
-	            maxArea = Math.max(maxArea, he * w );
-	            i--;
-	        }
-	    }
-	    return maxArea;
-	}
+        if( matrix == null || matrix.length == 0 ) {
+            return 0;
+        }
+        
+        int row = matrix.length;
+        int col = matrix[0].length;
+        
+        int[] heights = new int[ col ];
+        for(int i = 0; i < col; i++ ) {
+            heights[i] = matrix[0][i] - '0';
+        }
+        
+        int result = maxRectangle(heights);
+        
+        for(int i = 1; i < row; i++ ) {
+            resetHeight(heights, matrix, i );
+            result = Math.max(result, maxRectangle(heights) );
+        }
+        
+        return result;
+    }
+    
+    private void resetHeight(int[] heights, char[][] matrix, int row ) {
+        for(int i = 0; i < matrix[0].length; i++ ) {
+            if( matrix[row][i] == '0' ) {
+                heights[i] = 0;
+            } else {
+                heights[i]++;
+            }
+        }
+    }
+    
+    private int maxRectangle(int[] heights ) {
+        Stack<Integer> stack = new Stack<>();
+        
+        int max = 0;
+        for(int i = 0; i <= heights.length; i++ ) {
+            int cur = i < heights.length ? heights[i] : 0;
+            while(!stack.isEmpty() && cur < heights[ stack.peek() ] ) {
+                int h = heights[ stack.pop() ];
+                int w = stack.isEmpty() ? i : i - stack.peek() - 1;
+                max = Math.max(max, h*w);
+            }
+            stack.push(i);
+        }
+        
+        return max;
+    }
 }
