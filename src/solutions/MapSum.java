79dae40964e22d9359c711e90b4163b77e598116
Zhiyuan <Zhiyuan@Lenovo-PC>
2018-01-17 22:47:19 -0500

    add and update solution

---
+++ src/solutions/MapSum.java
@@ -0,0 +1,30 @@
+package solutions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapSum {
+private Map<String, Integer> map;
+    
+    public MapSum() {
+        map = new HashMap<>();    
+    }
+    
+    public void insert(String key, int val) {
+        map.put(key, val);
+    }
+    
+    public int sum(String prefix) {
+        
+        int count = 0;
+        
+        for (String key : map.keySet()) {
+            
+            String keyPrefix = prefix.length() > key.length() ? key : key.substring(0, prefix.length());
+            if (keyPrefix.equals(prefix))
+                count += map.getOrDefault(key, 0);
+        }
+        
+        return count;
+    }
+}
--- src/solutions/PathSum.java
+++ src/solutions/PathSum.java
@@ -3,6 +3,7 @@ package solutions;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import util.TreeNode;
 
@@ -117,4 +118,44 @@ public class PathSum {
         return hasPathSum( root.left, sum) || hasPathSum( root.right, sum);
         
     }
+    
+    int sum = 0;
+    Map<Integer, Integer> tree = new HashMap<>();
+    /**
+     * Path Sum IV
+     * @param nums
+     * @return
+     */
+    public int pathSum4(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        
+        for (int num : nums) {
+            int key = num / 10;
+            int value = num % 10;
+            tree.put(key, value);
+        }
+        
+        traverse(nums[0] / 10, 0);
+        
+        return sum;
+    }
+    
+    private void traverse(int root, int preSum) {
+        int level = root / 10;
+        int pos = root % 10;
+        int left = (level + 1) * 10 + pos * 2 - 1;
+        int right = (level + 1) * 10 + pos * 2;
+        
+        int curSum = preSum + tree.get(root);
+        
+        if (!tree.containsKey(left) && !tree.containsKey(right)) {
+            sum += curSum;
+            return;
+        }
+        
+        if (tree.containsKey(left)) traverse(left, curSum);
+        if (tree.containsKey(right)) traverse(right, curSum);
+    }
+    
+    
 }
