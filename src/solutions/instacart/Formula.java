7ae1d673d7b882a61a36fb91a86ee9e6077571fe
Zhiyuan Cui <zhiyuanc@zillowgroup.com>
2021-05-31 19:32:45 -0700

    polish code

--- src/solutions/instacart/Formula.java
+++ src/solutions/instacart/Formula.java
@@ -1,16 +1,28 @@
 package solutions.instacart;
 
+import org.junit.Assert;
+
 import java.util.*;
 
+/**
+ * Instacart onsite 面经
+ */
 public class Formula {
 
 
-
-    public int question1(String answer, String[] input) throws Exception{
+    /**
+     * 第一问：第一小问 expression:["T2", ["T1 = 1", "T2 = T3", "T3 = T1"]] output: T2值
+     * 公式都是左边一个变量， 右边是变量或者数值
+     * @param target
+     * @param expression
+     * @return
+     * @throws Exception
+     */
+    public int question1(String target, String[] expression) throws Exception{
         Map<String, String> formulaMapping = new HashMap<>();
 
-        for(int i = 0; i < input.length; i++) {
-            String formula = input[i];
+        for(int i = 0; i < expression.length; i++) {
+            String formula = expression[i];
 
             formula = formula.replaceAll("\\s+","");
             String[] parts = formula.split("=");
@@ -18,12 +30,10 @@ public class Formula {
             formulaMapping.put(parts[0], parts[1]);
         }
 
-        return dfs1(answer, formulaMapping);
+        return dfs1(target, formulaMapping);
     }
 
     private int dfs1(String key, Map<String, String> formulaMapping) throws Exception{
-
-
         if(!formulaMapping.containsKey(key)) {
             throw new Exception("IMPOSSIBLE");
         }
@@ -36,11 +46,20 @@ public class Formula {
         }
     }
 
-    public int question2WithDFS(String answer, String[] input) throws Exception {
+    /**
+     * 第二小问 expression: ["T2", ["T1 = 1", "T2 = 2 + T4", "T3 = T1 - 4", "T4 = T1 + T3]]
+     * output:T2值
+     * 公式左边为变量，右边为一个或多个变量/数值，包括加减操作
+     * @param target
+     * @param expression
+     * @return
+     * @throws Exception
+     */
+    public int question2WithDFS(String target, String[] expression) throws Exception {
         Map<String, String> formulaMapping = new HashMap<>();
 
-        for(int i = 0; i < input.length; i++) {
-            String formula = input[i];
+        for(int i = 0; i < expression.length; i++) {
+            String formula = expression[i];
 
             formula = formula.replaceAll("\\s+","");
             String[] parts = formula.split("=");
@@ -51,9 +70,7 @@ public class Formula {
         //DFS
         Map<String, Integer> cache = new HashMap<>();
         Set<String> visited = new HashSet<>();
-        return dfs(answer, formulaMapping, cache, visited);
-
-
+        return dfs(target, formulaMapping, cache, visited);
     }
 
     private int dfs(String key, Map<String, String> formulaMapping, Map<String, Integer> cache, Set<String> visited) throws Exception{
@@ -67,10 +84,10 @@ public class Formula {
         String formula = formulaMapping.get(key);
 
         formula = formula.replaceAll("\\s+","");
+        int len = formula.length();
 
         int sign = 1;
         int result = 0;
-        int len = formula.length();
         for(int i = 0; i < len; i++) {
             if(Character.isDigit(formula.charAt(i))) {
                 int sum = formula.charAt(i) -'0';
@@ -81,6 +98,8 @@ public class Formula {
                 result += sign * sum;
             } else if(Character.isLetter(formula.charAt(i))) {
                 String nextKey = formula.substring(i, i+2);
+                //如果是extra follow up
+                //String nextKey = findKey(formula, i);
                 int value = dfs(nextKey, formulaMapping, cache,visited);
                 if(!cache.containsKey(nextKey)) {
                     cache.put(nextKey, value);
@@ -97,17 +116,41 @@ public class Formula {
         return result;
     }
 
+    /**
+     * 变量是多个字母  "AA1", ["AA1 = AB1 + 1", "AB1=1"] => "2"
+     * @param formula
+     * @param index
+     * @return
+     */
+
+    private String findKey(String formula, int index) {
+        int start = index;
+
+        for(int i = index+1; i < formula.length(); i++) {
+            if(Character.isLetter(formula.charAt(i))) {
+                continue;
+            } else if(Character.isDigit(formula.charAt(i))) {
+                return formula.substring(start, i+1);
+            }
+        }
+
+        return null;
+    }
+
 
     public static void main(String[] args) {
         Formula f = new Formula();
 
         try {
-            System.out.println(f.question1("T2",new String[]{"T1 = 1", "T2 = T3", "T3 = T1"}));
-            System.out.println(f.question2WithDFS("T2", new String[]{"T1 = 1", "T2 = 2 + T4", "T3 = T1 - 4", "T4 = T1 + T3"}));
+            Assert.assertEquals(1,f.question1("T2",new String[]{"T1 = 1", "T2 = T3", "T3 = T1"}));
+            Assert.assertEquals(0, f.question2WithDFS("T2", new String[]{"T1 = 1", "T2 = 2 + T4", "T3 = T1 - 4", "T4 = T1 + T3"}));
+            Assert.assertEquals(10, f.question2WithDFS("T3", new String[]{"T1 = 4", "T2 = 3", "T3 = T4 + T5", "T4=T1-T2+2", "T5 = T4 + T1"}));
+            Assert.assertEquals(-25, f.question2WithDFS("T3", new String[]{"T1 =-3", "T2 = 8", "T3 = +T4 +T5", "T4 = T1- T2", "T5 = T4 + T1"}));
             //加visited
-            System.out.println(f.question2WithDFS("T2", new String[]{"T1 = 1", "T1 = T2 + 2", "T2 = 4 - T1"}));
+            //f.question2WithDFS("T3", new String[]{"T1 = T2", "T2 = T3", "T3 = T1"});
+            //f.question2WithDFS("T2", new String[]{"T1 = 1", "T1 = T2 + 2", "T2 = 4 - T1"});
         } catch(Exception e) {
-            System.out.println(e.getMessage());
+            e.printStackTrace();
         }
     }
 }
